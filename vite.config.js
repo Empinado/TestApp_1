06bc34e8f767fbbbc619c8eaec68b0c0a542941a
Alexandr Degtyarev <Sasha20111997@gmail.com>
2025-04-18 18:46:10 +0500

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/project/", // Замените на имя вашего репозитория
+  base: "/testapp_1/", // Замените на имя вашего репозитория
 });
